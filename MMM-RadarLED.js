7c8bdd049d12e7b529b8a06d5c037398c8ee1a09
Tobias Buss <tobi@derbuss.de>
2024-07-16 22:27:45 +0200

    reformat

--- MMM-RadarLED.js
+++ MMM-RadarLED.js
@@ -1,18 +1,21 @@
 Module.register("MMM-RadarLED", {
-  defaults: {
-    radarSensorPin: 17,  // GPIO pin for the radar sensor
-    wledIpAddress: "192.168.1.100",  // IP address of the WLED controller
-    presenceTimeout: 5,  // Time in seconds before sending notification
-  },
-
-  start: function() {
-    Log.info("Starting module: " + this.name);
-    this.sendSocketNotification("CONFIG", this.config);
-  },
-
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === "USER_PRESENCE") {
-      this.sendNotification("USER_PRESENCE");
+    defaults: {
+      radarSensorPin: 17,  // GPIO pin for the radar sensor signal
+      wledIpAddress: "192.168.1.100",  // IP address of the WLED controller
+      presenceTimeout: 5,  // Time in seconds before sending notification
+      serialPort: "/dev/ttyS0",  // Serial port for RX/TX communication
+      baudRate: 115200  // Baud rate for serial communication
+    },
+  
+    start: function() {
+      Log.info("Starting module: " + this.name);
+      this.sendSocketNotification("CONFIG", this.config);S
+    },
+  
+    socketNotificationReceived: function(notification, payload) {
+      if (notification === "USER_PRESENCE") {
+        this.sendNotification("USER_PRESENCE");
+      }
     }
-  }
-});
+  });
+  
